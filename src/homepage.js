1d3b4701f90651cb5bf9aa0882f20222b72f9163
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-06 21:04:30 +0200

    Add homepage.js, renders homepage into the DOM

---
+++ src/homepage.js
@@ -0,0 +1,17 @@
+import cafeImg from './images/cafe.png';
+
+export function renderHomepage() {
+    const content = document.querySelector('#content');
+    const welcomeText = document.createElement('div');
+    welcomeText.textContent = 'Welcome to Coffee Talk Cafe!';
+
+    const image = document.createElement('img');
+    image.src = cafeImg;
+
+    const description = document.createElement('div');
+    description.textContent = 'Site under construction, please stand by.';
+
+    content.append(welcomeText, image, description);
+
+    return content;
+}
