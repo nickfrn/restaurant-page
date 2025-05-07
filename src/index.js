9ca76c695d1737d415b0c8420a740c74d40066e2
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-07 19:36:03 +0200

    Add logic for updating content div

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import { renderHomepage } from "./homepage.js";
+import { renderMenu } from "./menu.js";
+import { renderAbout } from "./about.js";
+
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+const about = document.querySelector('#about');
+const content = document.querySelector('#content');
 
 renderHomepage();
+
+home.addEventListener('click', () => {
+    content.innerHTML = '';
+
+    renderHomepage();
+}); 
+menu.addEventListener('click', () => {
+    content.innerHTML = '';
+
+    renderMenu();
+});
+about.addEventListener('click', () => {
+    content.innerHTML = '';
+
+    renderAbout();
+});
