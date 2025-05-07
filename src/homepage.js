bc09b22501436d96497e9b80ef31ecd6f3e4e3f8
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-07 21:11:39 +0200

    Add some styling

--- src/homepage.js
+++ src/homepage.js
@@ -1,17 +1,16 @@
-import cafeImg from './images/cafe.jpg';
+import "./style.css";
 
 export function renderHomepage() {
     const content = document.querySelector('#content');
     const welcomeText = document.createElement('div');
-    welcomeText.textContent = 'Welcome to Coffee Talk Cafe!';
-
-    const image = document.createElement('img');
-    image.src = cafeImg;
+    welcomeText.textContent = 'Welcome to our very original Cafe!';
+    welcomeText.style.backgroundColor = 'rgba(0, 0, 0, .4)';
 
     const description = document.createElement('div');
-    description.textContent = 'Site under construction, please stand by.';
+    description.textContent = 'Site under construction, please stand by. (I\'ll probably style the site when I\'m done with TOP)';
+    description.style.backgroundColor = 'rgba(0, 0, 0, .4)';
 
-    content.append(welcomeText, image, description);
+    content.append(welcomeText, description);
 
     return content;
 }
