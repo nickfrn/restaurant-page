284cd7e7db9e615ccf6a87f3924d38225ee2af77
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-05-07 19:36:23 +0200

    Add menu.js

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+export function renderMenu() {
+    const content = document.querySelector('#content');
+    const test = document.createElement('h1');
+    test.textContent = 'TEST';
+
+    content.appendChild(test);
+
+    return content;
+}
